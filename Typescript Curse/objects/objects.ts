fc1964168245bd250712f66473a90c925562f28f
Ricardo Velez <ricardovelecal@gmail.com>
2022-11-28 14:13:36 -0600

    objects folder added

--- Typescript Curse/objects/objects.ts
+++ Typescript Curse/objects/objects.ts
@@ -3,7 +3,10 @@
     let flash: {name: string, age: number, powers: string[], getName?: () => string} = {
         name: 'Barri Alen',
         age: 24,
-        powers: ['Super Speed', 'Time Travel']
+        powers: ['Super Speed', 'Time Travel'],
+        getName(): string{
+            return this.name;
+        }
     }
 
     let superman: {name: string, age: number, powers: string[], getName?: () => string} = {
