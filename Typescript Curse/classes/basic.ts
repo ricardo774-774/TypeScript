f27bdb69044db803c7bd71bd0d08d4884ebe708f
Ricardo Velez <ricardovelecal@gmail.com>
2022-11-28 14:14:58 -0600

    classes folder added

--- Typescript Curse/classes/basic.ts
+++ Typescript Curse/classes/basic.ts
@@ -2,25 +2,29 @@
 
     class Athlete {
 
+        // DEFINICION EN DES-USO
         // private name: string;
         // public country: string;
         // public gender?: string;
+
+        // static son atributos predefinidos 
         static human: boolean = true;
         static getClass() { return `Class name: ${this.name}`}
 
+        // DEFINICION de atributos 
         constructor(
             private name: string, 
             public country: string, 
             public gender?: string,
         ) {}
 
-        getData() {
+        public getData() {
             console.log(`Name: ${this.name}`);
             console.log(`Country: ${this.country}`);
             console.log(`Gender: ${this.gender}`);
         }
 
-    };
+    }
 
     const Froning: Athlete = new Athlete('Froning', 'USA', 'Male');
     console.log(Froning);
--- Typescript Curse/classes/extends.ts
+++ Typescript Curse/classes/extends.ts
@@ -1,38 +1,46 @@
 (() => {
+    // clase padre
     class athlete {
+        // atributos 
         constructor(
             public name: string,
             public age: number,
         ) {
             console.log('Constructor athlete called');
         }
-
+        // protected solo nos permite utilizar dentro de la clase o sus hijos, no fuera
         protected getData() {
             return `Name: ${this.name} and Age: ${this.age}`;
         }
     };
-
+    // clase que hereda de athlete
     class athleteSoccer extends athlete {
+        // constructor clase hijo
         constructor(
             name: string,
             age: number,
             public goldenBall: boolean,
         ){
+            // datos clase padre
             super(name, age)
             console.log('Constructor soccer athlete called');
         }
 
+        // tiene que retornar algo
         get getName() {
             return `My name is ${this.name}`;
         }
 
+        // solo puede resivir un valor 
+        // no tiene que retornar nada
         set setName( name: string ) {
+            // evalua si el nombre es menor a 3
             if(name.length < 3) {
                 throw new Error('The name must have more than 3 letters');
             }
             this.name = name;
         }
-
+        // llama al metodo de su padre
         public getDataSoccer() {
             console.log( this.getData() );
         }
