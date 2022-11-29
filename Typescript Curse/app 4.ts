06368671964b0387b65d43e29f5f2edcd90d4035
Ricardo Velez <ricardovelecal@gmail.com>
2022-11-28 18:22:59 -0600

    app 4.ts added

---
+++ Typescript Curse/app 4.ts
@@ -0,0 +1,87 @@
+// Crear interfaces
+
+// Cree una interfaz para validar el auto (el valor enviado por parametro)
+
+interface Auto {
+  encender: boolean;
+  velocidadMaxima: number;
+  acelear(): void;
+}
+
+const conducirBatimovil = ( auto: Auto ):void => {
+  auto.encender = true;
+  auto.velocidadMaxima = 100;
+  auto.acelear();
+}
+
+const batimovil: Auto = {
+  encender:false,
+  velocidadMaxima:0,
+  acelear(){
+    console.log("...... gogogo!!!");
+  }
+}
+
+// Cree una interfaz con que permita utilzar el siguiente objeto
+// utilizando propiedades opcionales
+
+interface Guason {
+  reir?: boolean;
+  comer?:boolean;
+  llorar?: boolean;
+}
+
+const guason: Guason = {
+  reir: true,
+  comer:true,
+  llorar:false
+}
+
+const reir = ( guason: Guason ):void => {
+  if( guason.reir ){
+    console.log("JAJAJAJA");
+  }
+}
+
+
+// Cree una interfaz para la siguiente funcion
+
+interface CiudadGoticaFn{
+  (ciudadanos:string[]): number;
+}
+
+const ciudadGotica: CiudadGoticaFn = ( ciudadanos:string[] ):number => {
+  return ciudadanos.length;
+}
+
+// Cree una interfaz que obligue crear una clase
+// con las siguientes propiedades y metodos
+
+/*
+  propiedades:
+    - nombre
+    - edad
+    - sexo
+    - estadoCivil
+    - imprimirBio(): void // en consola una breve descripcion.
+*/
+
+interface Human {
+  nombre: string;
+  edad: number;
+  sexo: string;
+  estadoCivil: string;
+  imprimirBio(): void;
+}
+
+class Persona implements Human {
+  public nombre: string;
+  public edad: number;
+  public sexo: string;
+  public estadoCivil: string;
+
+  public imprimirBio(): void {
+    console.log(`This is the bio of ${ this.nombre }`)
+  }
+}
+
